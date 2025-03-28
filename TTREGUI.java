45c139a8f23cf560709cf9b5269b10a533b0ec00
DanielKim1010 <k2113321@students.katyisd.org>
2025-03-28 11:08:50 -0500

    Add files via upload

--- TTREGUI.java
+++ TTREGUI.java
@@ -14,10 +14,19 @@ import javax.swing.JPanel;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
 public class TTREGUI extends JPanel{
+	private BufferedImage gamebg,gameboard;
 	public TTREGUI(GameFrame frame) {
-		
+		this.setLayout(null);
+		try {
+			gamebg = ImageIO.read(TTREGUI.class.getResource("/images/gamebg.jfif"));
+			gameboard = ImageIO.read(TTREGUI.class.getResource("/images/gameboard.PNG"));
+		}
+		catch(Exception e) {
+			System.out.println("TTRE GUI image issue");
+		}
 	}
 	public void paintComponent(Graphics g) {
-		System.out.println("success");
+		g.drawImage(gamebg, 0, 0, getWidth(), getHeight(), null);
+		g.drawImage(gameboard, 0, 0, (int)(getWidth()*0.73719), (int)(getHeight()*0.87704), null);
 	}
 }
