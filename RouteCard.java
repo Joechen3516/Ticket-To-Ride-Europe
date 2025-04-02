1ac6e4157f79cba2d48800877d06125001a4c8a8
Kevin21140 <k2114037@students.katyisd.org>
2025-04-02 11:00:37 -0500

    Update RouteCard.java

--- RouteCard.java
+++ RouteCard.java
@@ -1,3 +1,40 @@
+import java.awt.image.BufferedImage;
+
 public class RouteCard {
+	private int points;
+	private City a;
+	private City b; 
+	private boolean longRoute = false;
+	private BufferedImage image; 
+	
+	
+	
+	public RouteCard(City a, City b, int points, boolean longRoute, BufferedImage image) { 
+		this.a = a;
+		this.b = b;
+		this.points = points;
+		this.longRoute = longRoute;
+		this.image = image; 
+	}
+	public RouteCard(City a, City b, int points, BufferedImage image) { 
+		this.a = a;
+		this.b = b;
+		this.points = points;
+		this.image = image; 
+		
+	}
+	public boolean isLong() {
+		
+		return longRoute;
+	}
+	public int getPoints() {
+		return points; 
+		
+	}
+	
+	public BufferedImage getImage() {
+		return image; 
+	}
+	
 
 }
