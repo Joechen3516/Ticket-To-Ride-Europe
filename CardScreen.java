a8242794520c8d66f7c0f77d71acbd8eb279dbae
Kevin21140 <k2114037@students.katyisd.org>
2025-04-02 11:19:04 -0500

    Update CardScreen.java

--- CardScreen.java
+++ CardScreen.java
@@ -149,19 +149,20 @@ public class CardScreen extends JPanel{
 		g.drawString(message, 10, 100);
 		g.drawString("You must keep at least 2 cards", 10, 175);
 		g.drawString(message1, (int)(getWidth() * 0.86), (int)(getHeight()*0.85));
-		int one = (int)(Math.random() * routeImages.length);
+		int one = ((int)(Math.random() * routeImages.length))-1;
+		
 		g.drawImage(routeImages[one], getWidth()/10, getHeight()/5, getWidth()/5, getHeight()/5, null);
 		BufferedImage[] x1 = removeImage(routeImages, one);
-		int two = (int)(Math.random() * routeImages.length);
+		int two = ((int)(Math.random() * routeImages.length))-1;
 		g.drawImage(x1[two], 7*getWidth()/20, getHeight()/5, getWidth()/5, getHeight()/5, null);
 		BufferedImage[] x2=removeImage(routeImages, two);
-		int three = (int)(Math.random() * routeImages.length);
+		int three = ((int)(Math.random() * routeImages.length))-1;
 		g.drawImage(x2[three], 6*getWidth()/10, getHeight()/5, getWidth()/5, getHeight()/5, null);
 		BufferedImage[] x3=removeImage(routeImages, three);
-		int four = (int)(Math.random() * routeImages.length);
+		int four = ((int)(Math.random() * routeImages.length))-1;
 		g.drawImage(x3[four], getWidth()/10, 3*getHeight()/5, getWidth()/5, getHeight()/5, null);
 		BufferedImage[] x4 = removeImage(routeImages, four);
-		int five = (int)(Math.random() * routeImages.length);
+		int five = ((int)(Math.random() * routeImages.length))-1;
 		g.drawImage(x4[(int)(Math.random() * routeImages.length)], 6*getWidth()/10, 3*getHeight()/5, getWidth()/5, getHeight()/5, null);
 		BufferedImage[] x5 = removeImage(routeImages, five);
 		//prevents duplicate cards
