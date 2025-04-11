705788b930f5723d0dc46bf07dd48db2d21cc91c
RedSmile32 <zonzeluc2@gmail.com>
2025-04-11 10:58:14 -0500

    updated player class with new functions

--- Player.java
+++ Player.java
@@ -4,7 +4,12 @@ import java.util.HashMap;
 public class Player {
 	private HashMap<TrainColor,ArrayList<TrainCard>> hand;
 	private PlayerColor color;
-	private int score = 0;
+	//private int score = 0; //score should be calculated by controller i think? not sure.
+	private ArrayList<RouteCard> routeCards = new ArrayList<>();
+	private int numStations = 3;
+	private ArrayList<Road> roads = new ArrayList<>();
+	private ArrayList<City> stationLocations = new ArrayList<>();
+	private int trainPieces = 45; //double check value later.
 
 
 
@@ -33,8 +38,37 @@ public class Player {
 
 
 	public void giveRoute(RouteCard rc) {
-		// TODO Auto-generated method stub
+		routeCards.add(rc);
 		
 	}
+
+	public void addTrainCard(TrainCard c) {
+		ArrayList<TrainCard> placeholder = hand.get(c.getColor());
+		placeholder.add(c);
+		hand.put(c.getColor(), placeholder);
+	}
+
+	public boolean addRoad(Road r) {
+		if (trainPieces - r.getLength()[0] < 0) {
+			return false;
+		}
+		roads.add(r);
+		trainPieces -= r.getLength()[0];
+		return true;
+
+	}
+
+	public boolean addStation(City c) {
+		if (numStations == 0) {
+			return false;
+		}
+		stationLocations.add(c);
+		numStations --;
+		return true;
+	}
+	
+
+
+
 
 }
