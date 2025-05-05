68b3272eabb9b3987ca56d5332a3df434099515b
DanielKim1010 <k2113321@students.katyisd.org>
2025-05-05 10:50:08 -0500

    Add files via upload

---
+++ EndScreen.java
@@ -0,0 +1,17 @@
+import java.awt.Graphics;
+
+public class EndScreen extends JPanel implements SwitchablePanel{
+	private GameController game;
+	public EndScreen(GameController game) {
+		this.game = game;
+	}
+	public void paintComponent(Graphics g) {
+		
+	}
+	@Override
+	public void OnSwitchedTo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
